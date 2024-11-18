69252a92a3d7eb9bfba0346c40ee20454cdb6857
Fleur41 <Muriithikings456@gmail.com>
2024-11-18 18:10:59 +0300

    Initial

---
+++ app/src/main/java/com/example/juicetracker/ui/bottomsheet/ColorSpinnerRow.kt
@@ -0,0 +1,15 @@
+package com.example.juicetracker.ui.bottomsheet
+
+import android.view.View
+import android.widget.AdapterView
+
+
+class SpinnerAdapter(val onColorChange: (Int) -> Unit): AdapterView.OnItemSelectedListener{
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        onColorChange(position)
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
+    }
+}
